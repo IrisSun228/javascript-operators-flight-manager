39f9faeca2e1e38f9ac9dd8592ca742d350ac782
Iris Sun <irissun@live.com>
2023-02-06 16:57:11 -0500

    Section 1 Step 1 - Calculate the number of flights

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,15 @@
 function Flights() {
+    const calculateNumberOfFlights = (numOfPassengers, flightCapacity) => {
+        let numOfFlights = Math.floor(numOfPassengers / flightCapacity);
 
+        if (numOfPassengers / flightCapacity !== numOfFlights) {
+            numOfFlights++;
+        };
+
+        return numOfFlights;
+    }
+
+    return {calculateNumberOfFlights};
 }
 
+module.exports = Flights();
