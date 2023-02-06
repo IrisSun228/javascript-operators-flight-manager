0ddd084091cb8454e8e837b24b98f41878872155
Iris Sun <irissun@live.com>
2023-02-06 17:08:18 -0500

    Section 1 Step 2 Debug

--- logic/flights.js
+++ logic/flights.js
@@ -1,10 +1,10 @@
 function Flights() {
     const calculateNumberOfFlights = (numOfPassengers, flightCapacity) => {
-        if (numOfPassengers < 0) {
+        if (numOfPassengers < 0 || !(Number.isInteger(numOfPassengers))) {
             throw new Error("The number of passengers must be a positive integer value");
         };
 
-        if (flightCapacity < 0) {
+        if (flightCapacity < 0 || !(Number.isInteger(flightCapacity))) {
             throw new Error("The capacity of the flight must be a positive integer value")
         }
         
