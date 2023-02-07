d0e79a83b01a97c717ed261f812c72e66b7885fd
Iris Sun <irissun@live.com>
2023-02-07 14:37:07 -0500

    Section 1 Step 5 - Distribute passengers to flights

--- logic/passengers.js
+++ logic/passengers.js
@@ -9,6 +9,45 @@ function Passengers() {
         }
     }
 
+    const distributeAllSeatsToAllPassengers = (vipPassengers, regPassengers, flights, businessSeats, economySeats) => {
+        let seatAssignments = {
+            'vipBuisness' : 0,
+            'vipEconomy' : 0,
+            'regBusiness' : 0,
+            'regEconomy' : 0
+        }
+
+        //Adjust  total passengers <= total seats
+        const totalBS = businessSeats * flights;
+        const totalSeats = totalBS + economySeats * flights;
+
+        if (vipPassengers > totalSeats) {
+            vipPassengers = totalSeats;
+            regPassengers = 0;
+        } else if (vipPassengers + regPassengers > totalSeats) {
+            regPassengers = totalSeats - vipPassengers;
+        }
+
+        //Assigning seats to passengers
+        if (vipPassengers <= totalBS) {
+            seatAssignments.vipBuisness = vipPassengers;
+        } else {
+            seatAssignments.vipBuisness = totalBS;
+            seatAssignments.vipEconomy = vipPassengers - totalBS;
+        }
+
+        if (regPassengers <= totalBS - vipPassengers) {
+            seatAssignments.regBusiness = regPassengers;
+        } else if (vipPassengers <= totalBS) {
+            seatAssignments.regBusiness = totalBS - vipPassengers;
+            seatAssignments.regEconomy = regPassengers - (vipPassengers - totalBS)
+        } else {
+            seatAssignments.regEconomy = regPassengers;
+        }
+
+        return seatAssignments;
+    }
+
     return {checkFlightCapacity};
 }
 
