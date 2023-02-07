d6d91b09efc976c516aa1fa4ceddffc84a3c00e3
Iris Sun <irissun@live.com>
2023-02-06 19:55:46 -0500

    Section 1 Step 4 - Check the capacity of the flight

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,15 @@
 function Passengers() {
+    const checkFlightCapacity = (flightCapacity, passengersNumbersArray) => {
+        let totalPassengers = passengersNumbersArray.reduce((a, b) => {return a + b});
 
+        if (totalPassengers < flightCapacity) {
+            return totalPassengers;
+        } else {
+            throw new Error();
+        }
+    }
+
+    return {checkFlightCapacity};
 }
 
+module.exports = Passengers();
