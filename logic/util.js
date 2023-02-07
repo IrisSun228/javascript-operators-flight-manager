0a18d13c0f2a85f78cc7ca73a022d78deeb75743
Iris Sun <irissun@live.com>
2023-02-07 15:02:52 -0500

    Section 1 Step 6 - Check total number of distributed passengers

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,12 @@
 function Util() {
+    const calculateTotalDistributedPassengers = (seatAssignments) => {
+        return seatAssignments.vipPassengersWithBusinessSeats + 
+               seatAssignments.vipPassengersWithEconomySeats +
+               seatAssignments.regularPassengersWithBusinessSeats +
+               seatAssignments.regularPassengersWithEconomySeats;
+    }
 
+    return {calculateTotalDistributedPassengers}
 }
 
+module.exports = Util();
