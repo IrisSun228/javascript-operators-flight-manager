d673a1277e01269e2c94ff6334b9a9404b08de09
Iris Sun <irissun@live.com>
2023-02-07 16:41:57 -0500

    Section 2 Step 1 - Calculate the final price of a ticket

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,9 @@
 function Prices() {
+    const calculateFinalPrice = (basePrice, passengerTypeVariation, flightTypeVariation) {
+        return basePrice * (1 + passengerTypeVariation / 100) * (1 + flightTypeVariation / 100);
+    }
 
+    return {calculateFinalPrice};
 }
 
+module.exports = Prices();
